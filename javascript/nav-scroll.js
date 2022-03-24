5abc265bc02cc590e7f30b03f3def6dd2be4a702
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-03-24 15:33:43 -0300

    nav se achica a los 100px de scroll

--- javascript/nav-scroll.js
+++ javascript/nav-scroll.js
@@ -1,7 +1,7 @@
 const navbar = document.querySelector('.header__nav');
 window.onscroll = function () {
 	// pageYOffset or scrollY
-	if (window.pageYOffset > 200) {
+	if (window.pageYOffset > 100) {
 		navbar.classList.add('nav-colored')
 	} else {
 		navbar.classList.remove('nav-colored')
